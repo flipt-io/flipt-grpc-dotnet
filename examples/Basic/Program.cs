f9a90357153c6b079c1650f0f5b17edea17a0730
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-08-13 13:24:12 -0400

    chore: add example; rework lib

---
+++ examples/Basic/Program.cs
@@ -0,0 +1,21 @@
+﻿using Flipt;
+using Grpc.Net.Client;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:9000");
+            var client = new Flipt.Flipt.FliptClient(channel);
+            var result = client.ListFlags(new Flipt.ListFlagRequest());
+
+            System.Console.WriteLine("Available Flags:");
+            foreach (var flag in result.Flags)
+            {
+                System.Console.WriteLine(flag.Key);
+            }
+        }
+    }
+}
